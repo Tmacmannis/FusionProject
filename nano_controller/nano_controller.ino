a503c49a138b726bbe759ffab213c9dbcf973a0b
Tmacmannis <timmacmannis@yahoo.com>
2018-05-18 13:31:11 -0400

    Texted added

--- nano_controller/nano_controller.ino
+++ nano_controller/nano_controller.ino
@@ -1,51 +1,35 @@
 /* Notes
  *  
  *  Must unplug nano from pcb before programming
- *  2 data lines working, 8 lines will give 64 combinations.. plenty
+ *  2 data lines working, 8 lines will give 256 combinations.. plenty
+ *  
+ *  To Do:
+ *    - Check how BLEread behaves, if it is possible to reset pins back to low after receiving a command. 
+ *    - Add more data lines, 8 total would allow for 256 possible commands
+ *  Brightness Control: 
+ *    - Use 10% increments of brightness
+ *    - leaves you with 246 free data lines...
+ *    - Up and down brightness, implement a timed delay pin reset to null after each received ble command, assuming only prints BLEinput when a command is sent
  */
 
 #include <SoftwareSerial.h>
 //test 
 #include "FastLED.h"
-#include <Wire.h>
-#define LED_PIN     13
-#define NUM_LEDS    171
-#define BRIGHTNESS  200
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
-CRGB leds[NUM_LEDS];
 
-CRGB currentColor;
 int BLEinput;
 int BLEprev;
-int currentBrightness = 200;
-
-CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
-
-extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
-
 SoftwareSerial mySerial(0, 1); // RX, TX  
 
 void setup() {  
   Serial.begin(9600);
-
   mySerial.begin(9600);
   delay( 3000 ); // power-up safety delay
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness(  BRIGHTNESS );
-  currentPalette = RainbowColors_p;
-  currentBlending = LINEARBLEND;
   pinMode(19, OUTPUT);
   pinMode(18, OUTPUT);
 
   digitalWrite(19, LOW);
   digitalWrite(18, LOW);
 
-  //Wire.begin(8);                // join i2c bus with address #8
-  //Wire.onRequest(BLEselect);
-  //pinMode(13, OUTPUT);
 }
 
 void loop() {  
